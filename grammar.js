8a3468e5482f83a8f32c6160210cad835dd7d422
Jonas Kuball <jonas.kuball@lector.ai>
2024-04-03 10:27:52 +0200

    ✨ allow for comments in transactions

--- grammar.js
+++ grammar.js
@@ -4,29 +4,31 @@ module.exports = grammar({
     extras: $ => [], // manage whitespace manually
 
     rules: {
-        source_file: $ => repeat(choice($.day_entry, $.comment, "\n")),
+        source_file: $ => repeat(choice($.day_entry, seq($.comment, "\n"), "\n")),
 
-        day_entry: $ => seq(seq($.date, "\n"), repeat($.transaction)),
+        day_entry: $ => seq($.date, "\n", repeat($.transaction)),
 
         transaction: $ => seq(
             optional($._whitespace),
             $.account,
             /\s\s+|\t/,
-            optional($.amount)
+            optional($.amount),
+            optional($._whitespace),
+            optional($.comment),
+            "\n",
         ),
 
         account: $ => "wrk", // TODO: regex that does not clash with dates
 
         amount: $ => repeat1("."),
 
-        comment: $ => seq(choice("#", ";"), /.*\n/),
-
-        date: $ => seq($._single_date),
+        comment: $ => seq(choice("#", ";"), /.*/),
 
         _whitespace: $ => repeat1(choice(" ", "\t")),
 
         // NOTE: I took this date definition from the ledger parser.
         // https://github.com/cbarrete/tree-sitter-ledger
+        date: $ => seq($._single_date),
         _dsep: $ => /[-\.\/]/,
         _2d: $ => /\d{1,2}/,
         _4d: $ => /\d{4}/,
